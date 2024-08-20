ac3b88899d9e420504149b10576a895349184110
Master02002 <qrazak05@gmail.com>
2024-08-20 13:35:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,19 @@ bodyElement.addEventListener("mousemove", (event)=> {
         spanElement.remove();
     }, 3000);
 });
+
+
+bodyElement.addEventListener("touchmove", (event)=> {
+    const xPos = event.offsetX;
+    const yPos = event.offsetY;
+    const spanElement = document.createElement("span");
+    spanElement.style.left = xPos + "px";
+    spanElement.style.top = yPos + "px";
+    const size = Math.random()*100;
+    spanElement.style.width = size + 'px';
+    spanElement.style.height = size + 'px'
+    bodyElement.appendChild(spanElement);
+    setTimeout(() => {
+        spanElement.remove();
+    }, 3000);
+});
